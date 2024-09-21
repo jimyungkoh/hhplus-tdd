111d438c1cba3e11dbcf7eb98fd6cad7751adbf5
jimyungkoh <kojimyung1206@naver.com>
2024-09-22 00:27:41 +0900

    initial commit

---
+++ src/point/point.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PointService {
+  // TODO: 포인트 충전 기능 구현
+  charge() {}
+
+  // TODO: 포인트 사용 기능 구현
+  use() {}
+
+  // TODO: 포인트 조회 기능 구현
+  point() {}
+
+  // TODO: 포인트 내역 조회 기능 구현
+  histories() {}
+}
