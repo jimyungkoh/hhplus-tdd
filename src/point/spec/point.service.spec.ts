208d01a237b02f6845286e9f8587174632c87b62
jimyungkoh <kojimyung1206@naver.com>
2024-09-22 23:52:25 +0900

    test: 포인트 조회 기능(getPointBy) 성공 케이스 작성

--- src/point/spec/point.service.spec.ts
+++ src/point/spec/point.service.spec.ts
@@ -2,6 +2,7 @@ import { MockFactory, Test, TestingModule } from '@nestjs/testing';
 import InjectionToken from 'src/database/injection.token';
 import { PointHistoryRepository } from 'src/database/pointhistory/pointhistory.repository';
 import { UserPointRepository } from 'src/database/userpoint/userpoint.repository';
+import { UserPoint } from '../model/point.model';
 import { PointService } from '../point.service';
 import pointHistoryRepositoryMock from './pointhistory.repository.mock';
 import userPointRepositoryMock from './userpoint.repository.mock';
@@ -32,8 +33,31 @@ describe('PointService', () => {
     pointHistoryRepository = module.get(InjectionToken.PointHistoryRepository);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   // TODO: 포인트 조회 기능 테스트 작성
-  describe('[getPointBy] 포인트 조회 기능 테스트', () => {});
+  describe('[getPointBy] 포인트 조회 기능 테스트', () => {
+    test('존재하는 사용자 id에 대한 포인트 조회는 성공한다', () => {
+      // given
+      const userId = 1;
+
+      const userPointStub: UserPoint = {
+        id: 1,
+        point: 1000,
+        updateMillis: Date.now(),
+      };
+      userPointRepository.selectById.mockResolvedValue(userPointStub);
+
+      // when
+      const result = service.getPointBy(userId);
+      const expected = userPointStub;
+
+      // then
+      expect(result).resolves.toEqual(expected);
+    });
+  });
 
   // TODO: 포인트 내역 조회 기능 테스트 작성
   describe('[getHistoryBy] 포인트 내역 조회 기능 테스트', () => {});
