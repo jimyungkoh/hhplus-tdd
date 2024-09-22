833d0324ccef29e78156b51108f0470addb3e9a2
jimyungkoh <kojimyung1206@naver.com>
2024-09-23 00:16:16 +0900

    feat: 포인트 조회 기능 구현
    - getPointBy(포인트 조회 기능) 성공 케이스에 대응하는 포인트 조회 기능 구현
    - PointService에 getPointBy 메소드 추가
    - UserPointRepository 의존성 주입
    - PointHistoryRepository 의존성 주입

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,16 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import InjectionToken from 'src/database/injection.token';
+import { PointHistoryRepository } from 'src/database/pointhistory/pointhistory.repository';
+import { UserPointRepository } from 'src/database/userpoint/userpoint.repository';
+import { UserPoint } from './model/point.model';
 
 @Injectable()
 export class PointService {
+  constructor(
+    @Inject(InjectionToken.UserPointRepository)
+    private readonly userPointRepository: UserPointRepository,
+
+    @Inject(InjectionToken.PointHistoryRepository)
+    private readonly pointHistoryRepository: PointHistoryRepository,
+  ) {}
+
+  // TODO: 포인트 조회 기능 구현
+  getPointBy(userId: number): Promise<UserPoint> {
+    return this.userPointRepository.selectById(userId);
+  }
+
+  // TODO: 포인트 내역 조회 기능 구현
+  getHistoryBy(userId: number) {}
+
   // TODO: 포인트 충전 기능 구현
   charge() {}
 
   // TODO: 포인트 사용 기능 구현
   use() {}
-
-  // TODO: 포인트 조회 기능 구현
-  point() {}
-
-  // TODO: 포인트 내역 조회 기능 구현
-  histories() {}
 }
