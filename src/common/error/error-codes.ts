db51479e46c514ae60a85b443b3f4540f6e85228
jimyungkoh <kojimyung1206@naver.com>
2024-09-25 18:14:23 +0900

    refactor: 에러 코드 - 통합 - 포인트 충전 및 사용
    - 금액이 양수가 아닌 경우 발생하는 에러이므로, 코드 중복임

--- src/common/error/error-codes.ts
+++ src/common/error/error-codes.ts
@@ -10,14 +10,9 @@ export class ErrorCodes {
     '유효하지 않은 사용자 ID 입니다',
   );
 
-  static readonly INVALID_CHARGE_AMOUNT = new ErrorCodes(
+  static readonly INVALID_POINT_AMOUNT = new ErrorCodes(
     400,
-    '포인트 충전 금액은 0보다 커야 합니다',
-  );
-
-  static readonly INVALID_USE_AMOUNT = new ErrorCodes(
-    400,
-    '포인트 사용 금액은 0보다 커야 합니다',
+    '포인트 금액은 0보다 커야 합니다',
   );
 
   static readonly NOT_ENOUGH_POINT = new ErrorCodes(
