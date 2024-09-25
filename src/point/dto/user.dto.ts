59ef5e9e02f74c37b0ffebe7ad1f797f306ef262
jimyungkoh <kojimyung1206@naver.com>
2024-09-25 18:18:23 +0900

    refactor: 사용자 DTO - ID DTO - 명칭 변경
    - DTO의 명확한 의미 전달을 위해
      UserIdParam -> UserIdDto 로 명칭 변경

--- src/point/dto/user.dto.ts
+++ src/point/dto/user.dto.ts
@@ -1,7 +1,7 @@
 import { IsInt, Min } from 'class-validator';
 import { ErrorCodes } from 'src/common/error/error-codes';
 
-export class UserIdParam {
+export class UserIdDto {
   @IsInt({ message: ErrorCodes.INVALID_USER_ID.message })
   @Min(1, { message: ErrorCodes.INVALID_USER_ID.message })
   id: number;
