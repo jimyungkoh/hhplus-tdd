70c5719584139867fd10ee928c72621d5e60a0ed
jimyungkoh <kojimyung1206@naver.com>
2024-09-27 05:55:30 +0900

    feat: 전역 - 예외 처리 필터
    - GlobalExceptionFilter 클래스 구현
      - HttpException, ApplicationException, Error 타입별 처리
      - 표준화된 에러 응답 포맷 정의
    - main.ts에 GlobalExceptionFilter 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filter/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
