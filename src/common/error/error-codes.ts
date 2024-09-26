4cbee59dfb40906c501db8fbc8a18af6dfbe817b
jimyungkoh <kojimyung1206@naver.com>
2024-09-27 05:57:07 +0900

    docs: 에러 코드 - 문서화

--- src/common/error/error-codes.ts
+++ src/common/error/error-codes.ts
@@ -5,17 +5,29 @@ export class ErrorCodes {
   ) {}
 
   // Point
-  static readonly INVALID_USER_ID = new ErrorCodes(
+  /**
+   * 유효하지 않은 사용자 ID 오류
+   * @type {ErrorCodes}
+   */
+  static readonly INVALID_USER_ID: ErrorCodes = new ErrorCodes(
     400,
     '유효하지 않은 사용자 ID 입니다',
   );
 
-  static readonly INVALID_POINT_AMOUNT = new ErrorCodes(
+  /**
+   * 유효하지 않은 포인트 금액 오류
+   * @type {ErrorCodes}
+   */
+  static readonly INVALID_POINT_AMOUNT: ErrorCodes = new ErrorCodes(
     400,
     '포인트 금액은 0보다 커야 합니다',
   );
 
-  static readonly NOT_ENOUGH_POINT = new ErrorCodes(
+  /**
+   * 포인트 잔액 부족 오류
+   * @type {ErrorCodes}
+   */
+  static readonly NOT_ENOUGH_POINT: ErrorCodes = new ErrorCodes(
     400,
     '포인트 잔액이 부족합니다',
   );
